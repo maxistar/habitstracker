602d65c494f67010940db3ff4732239d1d8b6e35
Max Starikov <mstarikov@itemis.com>
2023-12-07 20:53:24 +0100

    add css

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
 //Required package
 var pdf = require("pdf-creator-node");
 var fs = require("fs");
+const path = require("path");
 const QRCode = require("qrcode");
 // Read HTML Template
 var html = fs.readFileSync("template.html", "utf8");
 
 var options = {
-    format: "A3",
+    format: "A4",
     orientation: "portrait",
     border: "10mm",
     header: {
-        height: "15mm",
+        height: "10mm",
         contents: '<div style="text-align: center;">Habit Calendar</div>'
     },
     footer: {
-        height: "28mm",
+        height: "10mm",
         contents: {
-            first: 'Cover page',
+            //first: 'Cover page',
             2: 'Second page', // Any page number is working. 1-based index
             default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
-            last: 'Last Page'
+            //last: 'Last Page'
         }
     }
 };
 
 let tableRows = [];
-const numberOfDays = 40;
+const numberOfDays = 30;
 const habits = [
     {name: "Habit 1"},
     {name: "Habit 2"},
@@ -54,6 +55,28 @@ async function createQRCode(url) {
     });
 }
 
+function fileResolve(relativePath) {
+    if (!relativePath) {
+        return null;
+    }
+    return "file://" + path.resolve(relativePath);
+}
+
+function convertFileToBase64(filePath) {
+    try {
+        // Read the file synchronously
+        const data = fs.readFileSync(filePath);
+
+        // Convert the file data to a base64 string
+        const base64String = data.toString('base64');
+
+        return base64String;
+    } catch (err) {
+        console.error(`Error reading file: ${err.message}`);
+        return null;
+    }
+}
+
 async function main() {
     const qrcode = await createQRCode('http://maxistar.ru');
 
@@ -62,7 +85,8 @@ async function main() {
         data: {
             qrcode: qrcode,
             tableRows: tableRows,
-            habits: habits
+            habits: habits,
+            cssFonts: fileResolve("./assets/style.css"),
         },
         path: "./output.pdf",
         type: "",
