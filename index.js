d69dd9fb4712b86a64f45df263756f258f518b5b
Max Starikov <mstarikov@itemis.com>
2023-12-07 14:28:40 +0100

    changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Required package
 var pdf = require("pdf-creator-node");
 var fs = require("fs");
-
+const QRCode = require("qrcode");
 // Read HTML Template
 var html = fs.readFileSync("template.html", "utf8");
 
@@ -10,8 +10,8 @@ var options = {
     orientation: "portrait",
     border: "10mm",
     header: {
-        height: "45mm",
-        contents: '<div style="text-align: center;">Author: Shyam Hajare</div>'
+        height: "15mm",
+        contents: '<div style="text-align: center;">Habit Calendar</div>'
     },
     footer: {
         height: "28mm",
@@ -24,34 +24,68 @@ var options = {
     }
 };
 
-var users = [
-    {
-        name: "Shyam",
-        age: "26",
-    },
-    {
-        name: "Navjot",
-        age: "26",
-    },
-    {
-        name: "Vitthal",
-        age: "26",
-    },
+let tableRows = [];
+const numberOfDays = 40;
+const habits = [
+    {name: "Habit 1"},
+    {name: "Habit 2"},
+    {name: "Habit 3"},
+    {name: "Habit 4"},
+    {name: "Habit 5"},
+    {name: "Habit 6"},
 ];
-var document = {
-    html: html,
-    data: {
-        users: users,
-    },
-    path: "./output.pdf",
-    type: "",
-};
 
-pdf
-    .create(document, options)
-    .then((res) => {
-        console.log(res);
-    })
-    .catch((error) => {
-        console.error(error);
+
+for(let i = 1; i < numberOfDays; i++) {
+    tableRows.push({day: i, habits: habits})
+}
+
+
+
+
+async function createQRCode(url) {
+    return new Promise((resolve) => {
+        QRCode.toDataURL(
+            url,
+            (err, url) => {
+                resolve(url);
+            }
+        )
     });
+}
+
+async function main() {
+    const qrcode = await createQRCode('http://maxistar.ru');
+
+    const document = {
+        html: html,
+        data: {
+            qrcode: qrcode,
+            tableRows: tableRows,
+            habits: habits
+        },
+        path: "./output.pdf",
+        type: "",
+    };
+    
+    pdf
+        .create(document, options)
+        .then((res) => {
+            console.log(res);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+
+}
+
+main()
+    .then()
+
+
+
+
+
+
+
+
